346128029da3782b4f120b3821d33e6583994cbd
Maxim Doodnikoff <mdoodnikoff@mail.ru>
2024-12-03 20:48:07 +0300

    20241203 Start version

---
+++ index.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/**
+ * F_N To visualize cyclic processes in the console
+ * @param {Number} index - The number of the cycle iteration
+ * @param {Count} count - The total amount of iterations
+ * @returns 
+ */
+module.exports = function processProgress(index, count) {
+    if (index || index === 0) {
+        if (index % 25 === 0) {
+            let perc = (index * 100 / count).toFixed(2)
+            perc = perc.length === 4 ? `0${perc}` : perc
+            let numb = perc.substring(0, 2)
+            let line = buildLine(parseInt(numb))
+
+            function buildLine(numb) {
+                let line = ``
+                let blockEmp = '▯▯▯▯▯ '
+                let block = {
+                    0: '▮▮▮▮▮ ',
+                    2: '▮▯▯▯▯ ',
+                    4: '▮▮▯▯▯ ',
+                    6: '▮▮▮▯▯ ',
+                    8: '▮▮▮▮▯ ',
+                }
+
+                if (numb < 10) {
+                    line = lastPart(numb)
+                    for (let index = 0; index < 9; index++) {
+                        line += blockEmp
+                    }
+                } else {
+                    numb = String(numb)
+                    let a = numb[0]
+                    let b = numb[1]
+                    line = `${firstPart(parseInt(a))}${lastPart(parseInt(b))}`
+                    for (let index = 0; index < 9 - parseInt(a); index++) {
+                        line += blockEmp
+                    }
+
+                }
+
+                function firstPart(numb) {
+                    let res = ''
+                    for (let index = 0; index < numb; index++) {
+                        res += block[0]
+                    }
+                    return res
+                }
+
+                function lastPart(numb) {
+                    let res = ''
+                    if (numb % 2 === 0) {
+                        numb === 0
+                            ? res = blockEmp
+                            : res = block[`${numb}`]
+
+                    } else {
+                        numb = Math.round(numb - 1)
+                        numb === 0
+                            ? res = blockEmp
+                            : res = block[`${numb}`]
+                    }
+                    return res
+                }
+
+                return line
+            }
+
+            console.log(`\x1b[92m ${perc}% \x1b[93m ${line} \x1b[0m `);
+            return perc
+        }
+    } else {
+        console.log(`\x1b[92m 100.00% ▮▮▮▮▮ ▮▮▮▮▮ ▮▮▮▮▮ ▮▮▮▮▮ ▮▮▮▮▮ ▮▮▮▮▮ ▮▮▮▮▮ ▮▮▮▮▮ ▮▮▮▮▮ ▮▮▮▮▮ \x1b[0m `);
+        return true
+    }
+}
